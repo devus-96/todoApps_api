af5a36f4b1a6c2d402c7e2844556bc98a43f8f6c
NDJOM richard <136581065+devus-96@users.noreply.github.com>
2025-01-06 17:49:00 +0100

    Update auth.php

--- auth/auth.php
+++ auth/auth.php
@@ -7,6 +7,8 @@ ici il sert a savoir si la méthode de récupération est GET*/
 
 /*il oui il utilise la createAuthUrl() pour créer l'URL de redirection vers la page Google d'authentification*/
 
+/* json_encode renvoie soit false soit un string représentant le json de la valeur entrée ici il renvoie '{authUrl : value}' au front grace au echo*/
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $authUrl = $gClient->createAuthUrl();
     echo json_encode(['authUrl' => $authUrl]);
