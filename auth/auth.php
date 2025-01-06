1f7671f1188696c12fb67a66efdcec2447319741
NDJOM richard <136581065+devus-96@users.noreply.github.com>
2025-01-06 17:44:53 +0100

    Update auth.php

--- auth/auth.php
+++ auth/auth.php
@@ -1,7 +1,11 @@
-<?php 
+<?php
 
 require_once "../action/config.php";
 
+/*$_SERVER est un tableau contenant des informations telles que les en-têtes, les chemins et les emplacements des scripts.
+ici il sert a savoir si la méthode de récupération est GET*/
+
+/*il oui il utilise la createAuthUrl() pour créer l'URL de redirection vers la page Google d'authentification*/
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $authUrl = $gClient->createAuthUrl();
