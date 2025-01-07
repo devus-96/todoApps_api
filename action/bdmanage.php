13e228f08734e511704375bfb5c2f1fee444daba
NDJOM richard <136581065+devus-96@users.noreply.github.com>
2025-01-07 18:32:31 +0100

    Update bdmanage.php

--- action/bdmanage.php
+++ action/bdmanage.php
@@ -3,9 +3,23 @@ class BDManage {
    private $user = "postgres";
    private $dbname = "appmanagebd";
    private $password = "daus985220";
+   private $pdo;
 
    
    function __construct() {
-        
+        try {
+          $this -> pdo = new PDO("pgsql:host=". "$this -> host". ";dbname=". "$this ->dbname". ", $user, $password");
+
+          $this -> pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+ 
+          $this -> pdo->setAttribute(PDO::ATTR_ORACLE_NULLS,PDO::NULL_EMPTY_STRING);
+
+          $this -> pdo->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
+
+       } catch (PDOException $e) {
+         die("Error: " . $e->getMessage());
+       }
     }
+
+
 }
